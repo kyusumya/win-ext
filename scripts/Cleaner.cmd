2557c945ef08e81b53e23b2780c81775d5e18172
きゅすみゃ <kyusumyalove@gmail.com>
2025-11-09 20:07:56 +0900

    Update Cleaner.cmd

--- scripts/Cleaner.cmd
+++ scripts/Cleaner.cmd
@@ -4,9 +4,4 @@ del /f /s /q "%windir%\Temp"
 del /f /s /q "%localappdata%\Temp"
 del /f /s /q "%windir%\Prefetch"
 
-for /f "usebackq delims=" %%L in (`wevtutil.exe el`) do (
-  echo Clearing: %%L
-  wevtutil.exe cl "%%L"
-)
-
 ipconfig /flushdns
