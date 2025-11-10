6a9d35ff00ce0a655f9d9c76776d120acdf5a949
きゅすみゃ <kyusumyalove@gmail.com>
2025-11-10 15:40:12 +0900

    Update Uninstall_Xbox.cmd

--- scripts/Uninstall_Xbox.cmd
+++ scripts/Uninstall_Xbox.cmd
@@ -1,6 +1,7 @@
-powershell "Get-AppxPackage -AllUsers "*Xbox" | Remove-AppxPackage"
+powershell "Get-AppxPackage -AllUsers "*Xbox*" | Remove-AppxPackage"
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\XboxNetApiSvc" /v "Start" /t REG_DWORD /d 4 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\XblGameSave" /v "Start" /t REG_DWORD /d 4 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\XblAuthManager" /v "Start" /t REG_DWORD /d 4 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\XboxGipSvc" /v "Start" /t REG_DWORD /d 4 /f
+
