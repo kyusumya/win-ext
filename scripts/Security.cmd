4945aa24c9af556dd4aad0479b5e8be7d8226e3c
きゅすみゃ <kyusumyalove@gmail.com>
2025-12-25 23:40:47 +0900

    Update Security.cmd

--- scripts/Security.cmd
+++ scripts/Security.cmd
@@ -1,7 +1,8 @@
 @echo off
-set "_SETSVC=.\scripts\_setSvc.cmd"
+set _SETSVC="%~1\_setSvc.cmd"
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d 1 /f
 
 call "%_SETSVC%" "upnphost" 4
+
 call "%_SETSVC%" "ShellHWDetection" 4
