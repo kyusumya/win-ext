71c9918d8f95e322dd6be88fbc4ba4766ee0aefb
きゅすみゃ <kyusumyalove@gmail.com>
2025-12-25 23:41:02 +0900

    Update _setsvc.cmd

--- scripts/_setsvc.cmd
+++ scripts/_setsvc.cmd
@@ -1,9 +1,10 @@
 @echo off
-SET SVCNAME=%~1
-SET STARTTYPE=%~2
+set SVCNAME=%~1
+set STARTTYPE=%~2
 
 reg query "HKLM\SYSTEM\CurrentControlSet\Services\%SVCNAME%" >nul 2>&1
 if %errorlevel% neq 0 ( exit /b 1 )
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\%SVCNAME%" /v Start /t REG_DWORD /d %STARTTYPE% /f
 sc stop "%SVCNAME%" >nul
+
